bb148d60a5a833e95622869d0b686b95979cef09
José Anderson <freefiremitodino@gmail.com>
2025-04-02 21:44:16 -0300

    versão 7

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.listaTarefa, name='lista-tarefa'),
-    path('tarefa/<int:id>', views.tarefaView, name='tarefa-view' ),
-    path('novaTarefa/', views.novaTarefa, name='nova-Tarefa'),
+    path('tarefa/<int:id>', views.tarefaView, name='tarefa-view'),
+    path('novaTarefa/', views.novaTarefa, name='nova-tarefa'),
+    path('edit/<int:id>', views.editTarefa, name='edit-tarefa'),
+    path('delete/<int:id>', views.deleteTarefa, name='delete-tarefa')
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -6,7 +6,7 @@ from .forms import TarefaForm
 
 def listaTarefa(request):
     tarefas = Tarefas.objects.all().order_by('-created_at')
-    return render(request,'tarefas/list.html', {'tarefas':tarefas})
+    return render(request, 'tarefas/list.html', {'tarefas':tarefas})
 
 def tarefaView(request, id):
     tarefa = get_object_or_404(Tarefas, pk=id)
@@ -21,7 +21,30 @@ def novaTarefa(request):
             tarefa.status = 'andamento'
             tarefa.save()
             return redirect('/')
-        
+
     else:
         form = TarefaForm()
         return render(request, 'tarefas/addTarefa.html', {'form':form})
+    
+
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefaForm(instance=tarefa)
+
+    if(request.method == 'POST'):
+        form = TarefaForm(request.POST, instance=tarefa)
+
+        if(form.is_valid()):
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    
+    else:
+        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    
+
+def deleteTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    tarefa.delete()
+    return redirect('/')
